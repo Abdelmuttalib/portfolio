95833fbf7ad399d089823f45f7d55cc8ebb39978
abdelmuttalib <hagar9886@gmail.com>
2022-02-15 23:53:34 +0800

    add page theme provider wrapping all components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from "next/app"
 import Head from "next/head"
+
 import "../styles/tailwind.scss"
+import { ThemeProvider } from "@components/ThemeContext"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,11 +11,13 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>Abdelmuttalib Ahmed | Frontend Developer</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className="relative px-10 sm:px-20 md:px-32 lg:px-44 xl:px-56 dark:bg-primary-700 transition-all">
-        <div className="flex flex-col items-center flex-1 min-h-screen">
-          <Component {...pageProps} />
+      <ThemeProvider>
+        <div className="relative px-10 sm:px-20 md:px-32 lg:px-44 xl:px-56 dark:bg-primary-700 transition-all">
+          <div className="flex flex-col items-center flex-1 min-h-screen">
+            <Component {...pageProps} />
+          </div>
         </div>
-      </div>
+      </ThemeProvider>
     </>
   )
 }
