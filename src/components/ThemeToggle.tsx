68a0d78c485b2c10dc3d39ca7eec44b319b8e4e3
abdelmuttalib <hagar9886@gmail.com>
2022-02-15 23:52:57 +0800

    add component to toggle theme

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,27 +1,25 @@
 import React from "react"
 
-import { FaSun, FaMoon } from "react-icons/fa"
-
+import MoonIcon from "../../public/svgs/MoonIcon.svg"
+import SunIcon from "../../public/svgs/SunIcon.svg"
 import { ThemeContext } from "./ThemeContext"
 
-const Toggle = () => {
+const ThemeToggle = () => {
   const { theme, setTheme } = React.useContext(ThemeContext)
 
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark")
+  }
+
   return (
-    <div className="p-2 rounded-full transition duration-500 ease-in-out">
-      {theme === "dark" ? (
-        <FaSun
-          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-          className="text-2xl text-gray-500 cursor-pointer dark:text-gray-400"
-        />
+    <div className="cursor-pointer ease-in-out duration-300">
+      {theme === "light" ? (
+        <SunIcon className="w-6 h-6 text-primary" onClick={toggleTheme} />
       ) : (
-        <FaMoon
-          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-          className="text-2xl text-gray-500 cursor-pointer dark:text-gray-400"
-        />
+        <MoonIcon className="w-6 h-6 text-primary-200" onClick={toggleTheme} />
       )}
     </div>
   )
 }
 
-export default Toggle
+export default ThemeToggle
