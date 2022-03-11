f7dafbe1745b384fe9f2478390a5e344c56cfee0
abdelmuttalib <hagar9886@gmail.com>
2022-03-11 11:05:14 +0800

    - add icon to with page title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <link rel="icon" href="/favicon.ico" />
         <title>Abdelmuttalib Ahmed | Frontend Developer</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
