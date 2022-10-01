4bf2476b77e127211557c95259214389e70ed233
Abdelmuttalib <hagar9886@gmail.com>
2022-10-01 16:51:42 +0800

    feat(config): remove unused files

--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Layout } from "./Layout/Layout";
-export { default as Personal } from "./Personal/Personal";
-export { default as Projects } from "./Projects/Projects";
-export { default as Experience } from "./Experience/Experience";
-export { default as ContactMe } from "./ContactMe/ContactMe";
