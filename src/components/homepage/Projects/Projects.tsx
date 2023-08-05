653c5cde049d59f9efae6dbfdf3b285be9324a4a
Abdelmuttalib <hagar9886@gmail.com>
2023-08-05 16:40:24 +0800

    feat(ui): new design

--- src/components/homepage/Projects/Projects.tsx
+++ src/components/homepage/Projects/Projects.tsx
@@ -6,23 +6,17 @@ import Project from "./Project";
 
 function Projects() {
   return (
-    <section className="layout grid gap-y-20 py-20 md:gap-y-56">
+    <section className="grid grid-cols-1 gap-y-14 gap-x-4 py-20 px-4 sm:px-6 md:grid-cols-2 md:gap-y-4">
       {projects.map(
         ({ title, technologies, image, link, live_link }, indx: number) => (
-          <>
-            <Project
-              key={indx}
-              projectTitle={title}
-              projectTechnologies={technologies || "React JS, Next JS"}
-              imagePath={image}
-              githubLink={link}
-              liveLink={live_link}
-            />
-
-            {indx < projects.length - 1 && (
-              <hr className=" border-primary-100/70 w-full" />
-            )}
-          </>
+          <Project
+            key={indx}
+            projectTitle={title}
+            projectTechnologies={technologies || "React JS, Next JS"}
+            imagePath={image}
+            githubLink={link}
+            liveLink={live_link}
+          />
         )
       )}
     </section>
